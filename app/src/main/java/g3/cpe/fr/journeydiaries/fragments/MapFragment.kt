860993a519e8a1130d772803390352a70a5d310e
Laurent Morel <laurent.morel30@gmail.com>
2018-10-22 10:27:48 +0200

    Maps

--- app/src/main/java/g3/cpe/fr/journeydiaries/fragments/MapFragment.kt
+++ app/src/main/java/g3/cpe/fr/journeydiaries/fragments/MapFragment.kt
@@ -9,6 +9,11 @@ import android.support.annotation.RequiresApi
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import g3.cpe.fr.journeydiaries.R
 import g3.cpe.fr.journeydiaries.databinding.FragmentMapBinding
 import g3.cpe.fr.journeydiaries.models.Journey
@@ -16,7 +21,18 @@ import g3.cpe.fr.journeydiaries.models.JourneyViewModel
 import g3.cpe.fr.journeydiaries.repositories.JourneysRepository
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     lateinit var journeysRepository: JourneysRepository
     var journeyId: Int = 0
@@ -29,6 +45,8 @@ class MapFragment : Fragment() {
         journeysRepository = JourneysRepository(context)
         val journey = loadJourney()
         binding.jvm = JourneyViewModel(journey)
+
+        //binding.root.mapView.getMapAsync(this)
 
         return binding.root
     }
